4265756481503017308e65d351c5605f802169d9
Yuliya-Nad <nad.yul2002@gmail.com>
2020-05-19 01:29:39 +0300

    бегает

--- pacman/Main.cpp
+++ pacman/Main.cpp
@@ -5,6 +5,7 @@
 #include <string>
 #include "map.h"
 using namespace sf;
+RenderWindow window(sf::VideoMode(800, 600), "Pacman");
 
 void inter_with_map(float dx, float dy, float time) {
 	float y = dy * time;
@@ -15,7 +16,6 @@ void inter_with_map(float dx, float dy, float time) {
 	for (int j = x/32; j < (x + w)/32; j++)
 	{
 		if (TileMap[i][j] == '0') {
-			std::cout << "HUY ";
 			if (dy > 0) {//go down
 				y = i * 32 - h;//stopped a hero
 			}
@@ -32,10 +32,10 @@ void inter_with_map(float dx, float dy, float time) {
 	}
 }
 
-float update(float time, int dir, float speed, Sprite spr_hero, float x, float y) //функция "оживления" объекта класса. update - обновление. принимает в себя время SFML , вследствие чего работает бесконечно, давая персонажу движение.
+float* update(float time, int dir, float speed, float x, float y)
 {
 	float dx, dy;
-	dx= dy = 0;
+	dx= dy = 0;	
 	switch (dir)
 	{	
 	case 0: dx = speed; dy = 0; break;//по иксу задаем положительную скорость, по игреку зануляем. получаем, что персонаж идет только вправо
@@ -43,13 +43,11 @@ float update(float time, int dir, float speed, Sprite spr_hero, float x, float y
 	case 2: dx = 0; dy = speed; break;//по иксу задаем нулевое значение, по игреку положительное. получается, что персонаж идет только вниз
 	case 3: dx = 0; dy = -speed; break;//по иксу задаем нулевое значение, по игреку отрицательное. получается, что персонаж идет только вверх
 	}
-
 	x += dx * time;//то движение из прошлого урока. наше ускорение на время получаем смещение координат и как следствие движение
 	y += dy * time;//аналогично по игреку
-
 	speed = 0;//зануляем скорость, чтобы персонаж остановился.
-	spr_hero.setPosition(x, y); //выводим спрайт в позицию x y , посередине. бесконечно выводим в этой функции, иначе бы наш спрайт стоял на месте.
-	return x, y;
+	float back[2] = { x, y };
+	return back;
 }
 
 int main() {
@@ -68,19 +66,20 @@ int main() {
 	spr_hero.setPosition(50, 30);
 
 	Clock clock;
-
+	float x, y;
+	x = y = 1;
 	while (window.isOpen()) {
 		float time = clock.getElapsedTime().asMicroseconds();
 		clock.restart();
 		time = time / 800;
 		Event event;
+		int dir = 0;
+		float speed = 0;
 		while (window.pollEvent(event)) {
 			if (event.type == Event::Closed) {
 				window.close();
 			}
 		}
-		float x, y;
-		x = y = 0;
 		for (int i = 0; i < HEIGHT_MAP; i++)
 		for (int j = 0; j < WIDTH_MAP; j++) {
 			if (TileMap[i][j] == ' ') spr_map.setTextureRect(IntRect(95, 0, 32, 32));
@@ -89,12 +88,25 @@ int main() {
 			spr_map.setPosition(j * 32, i * 32);
 			window.draw(spr_map);
 		}
-
-		if (Keyboard::isKeyPressed(Keyboard::Left)) { int dir = 1; float speed = 0.15; spr_hero.setTextureRect(IntRect(92, 49, -46, 46)); x,y=update(time, dir, speed, spr_hero, x, y); }
-		if (Keyboard::isKeyPressed(Keyboard::Right)) { int dir = 1; float speed = 0.15; spr_hero.setTextureRect(IntRect(46, 49, 46, 46)); x, y = update(time, dir, speed, spr_hero, x, y);}
-		if (Keyboard::isKeyPressed(Keyboard::Up)) { int dir = 1; float speed = 0.15;  spr_hero.setTextureRect(IntRect(46, 294, 46, -46)); x, y = update(time, dir, speed, spr_hero, x, y);}
-		if (Keyboard::isKeyPressed(Keyboard::Down)) { int dir = 1; float speed = 0.15;  spr_hero.setTextureRect(IntRect(46, 245, 46, 46)); x, y = update(time, dir, speed, spr_hero, x, y);}
-
+		if (Keyboard::isKeyPressed(Keyboard::Left)) {
+			dir = 1;
+			speed = 0.15; 
+			spr_hero.setTextureRect(IntRect(92, 49, -46, 46));}
+		if (Keyboard::isKeyPressed(Keyboard::Right)) {
+			dir = 0;
+			speed = 0.15;
+			spr_hero.setTextureRect(IntRect(46, 49, 46, 46));}
+		if (Keyboard::isKeyPressed(Keyboard::Up)) {
+			dir = 3;
+			speed = 0.15;
+			spr_hero.setTextureRect(IntRect(46, 294, 46, -46));}
+		if (Keyboard::isKeyPressed(Keyboard::Down)) {
+			dir = 2;
+			speed = 0.15;
+			spr_hero.setTextureRect(IntRect(46, 245, 46, 46));}
+		x = update(time, dir, speed, x, y)[0];
+		y = update(time, dir, speed, x, y)[1];
+		spr_hero.setPosition(x, y);
 		window.draw(spr_hero);
 		window.display();
 	}
